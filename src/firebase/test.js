6080f019df02c75da4043eaf25f9aeb39776e701
mayank agrawal <mayankagrawal@mayanks-MacBook-Air.local>
2021-03-06 14:13:38 +0530

    implemented firestor utils, including ability to store authenticated users into firestore databse

---
+++ src/firebase/test.js
@@ -0,0 +1,8 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
+const firestore = firebase.firestore();
+
+firestore.collection('users').doc('vSEy89RxKqKkEdiI1MxO').collection('cartItems').doc('Eo4b2AhNxMXYqtsJgtH8');
+firestore.doc('/users/vSEy89RxKqKkEdiI1MxO/cartItems/Eo4b2AhNxMXYqtsJg');
+firestore.collection('/users/vSEy89RxKqKkEdiI1MxO/cartItems');
